2f8501543f97ee29728405832b931f6512ac4067
Muhammad Sauqi Rizqullah <sauqi.risqullah@gmail.com>
2024-07-11 11:03:01 +0700

    [ADD] Menambahkan Entity dan DTO User Account

---
+++ src/main/java/constant/ConstantTable.java
@@ -0,0 +1,5 @@
+package constant;
+
+public class ConstantTable {
+    public static final String USER_ACCOUNT = "m_user_account";
+}
---
+++ src/main/java/entity/UserAccount.java
@@ -0,0 +1,36 @@
+package entity;
+
+import constant.ConstantTable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = ConstantTable.USER_ACCOUNT)
+public class UserAccount{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Invalid email format")
+    private String email;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+}
